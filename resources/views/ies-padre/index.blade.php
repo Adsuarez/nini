150f1925e91c8631af888be8f6000319a4c375bf
Adsuarez <adsuarezq@gmail.com>
2023-08-02 13:01:02 -0500

    create content document for instituciones

---
+++ resources/views/ies-padre/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Sección Instituciones</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab adipisci pariatur, nulla dicta modi explicabo recusandae ullam sunt sapiente repellendus deserunt est tenetur voluptas, fugiat in, possimus dolorem libero beatae?</p>
+            </div>
+        </div>
+    </div>
+@endsection
