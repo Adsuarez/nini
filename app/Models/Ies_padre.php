e764e1993b41830716227f68f9de34958380f37f
Adsuarez <adsuarezq@gmail.com>
2023-08-02 12:58:59 -0500

    update model information for ies_padre

--- app/Models/Ies_padre.php
+++ app/Models/Ies_padre.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Ies_padre extends Model
 {
     use HasFactory;
+    protected $table = 'ies_padre';
+    protected $primaryKey = 'codigo-institucion';
+    protected $fillable = ['codigo-institucion', 'nombre-institucion', 'siglas-institucion', 'logo-institucion', 'costo-inscripcion'];
 }
